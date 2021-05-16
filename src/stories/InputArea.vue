0152e5f9e365a925324783fe4bb3009ec7466e50
Jungduck Bae <neoniceguy@kobaltlab.com>
2021-05-16 22:02:08 +0900

    입력항목 라벨 + 인풋박스 컴포넌트

---
+++ src/stories/InputArea.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="area-box">
+    <label>{{ label }}</label> :
+    <input
+      type="number"
+      :placeholder="placeholder"
+      :min="min"
+      :max="max"
+      :maxlength="maxlength"
+      :value="value"
+      :name="name"
+      @input="typing"
+    />
+  </div>
+  <div class="valid" v-show="validShow">{{ validMessage }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, computed } from "vue";
+import "./InputArea.css";
+
+export default defineComponent({
+  name: "input-area",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    max: {
+      type: Number,
+      required: true,
+    },
+    min: {
+      type: Number,
+      required: true,
+    },
+    maxlength: {
+      type: Number,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    validMessage: {
+      type: String,
+      required: true,
+    },
+    validShow: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ["callParents"],
+  setup(props, { emit }) {
+    props = reactive(props);
+    const state = reactive({
+      validShow: props.validShow,
+    });
+
+    return {
+      state,
+      typing(e: any) {
+        if (e.target.value.length > e.target.maxLength) {
+          e.target.value = e.target.value.slice(0, e.target.maxLength);
+          state.validShow = false;
+        }
+
+        if (e.target.value) {
+          if (e.target.value > e.target.max) {
+            e.target.value = e.target.max;
+            state.validShow = false;
+          }
+
+          if (e.target.value < e.target.min) {
+            e.target.value = e.target.min;
+            state.validShow = false;
+          }
+        }
+
+        state.validShow = false;
+        emit("callParents", {
+          val: e.target.value,
+          name: e.target.name,
+          validShow: state.validShow,
+        });
+      },
+    };
+  },
+});
+</script>
