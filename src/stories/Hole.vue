f144232b3479087500367ad4324631e249f58103
Jungduck Bae <neoniceguy@kobaltlab.com>
2021-05-18 09:52:23 +0900

    두더지 컴포넌트 생성

---
+++ src/stories/Hole.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hole" :class="{ up : isUp }">
+      <div class="mole" @click="moleClick"></div>
+  </div>
+</template>
+
+<style scoped>
+@import "./Hole.css";
+</style>
+
+<script lang="ts">
+import { defineComponent, reactive, computed } from "vue";
+import MyButton from "../stories/Button.vue";
+
+export default defineComponent({
+  name: "Hole",
+  props: {
+      isUp: {
+          type: Boolean,
+          required: true,
+      }
+  },
+  emits: ["moleClick"],
+  setup(props, { emit }) {
+    props = reactive(props);
+    const state = reactive({
+
+    });
+
+    return {
+      state,
+      moleClick() {
+          emit('moleClick');
+      }    
+    };
+  },
+});
+</script>
