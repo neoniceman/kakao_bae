8c4a0f1b0f6aeb88e168e73d1fc4246642b1516e
Jungduck Bae <neoniceguy@kobaltlab.com>
2021-05-18 21:57:52 +0900

    esline fixed  적용

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,18 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-    colNumber: 0,
-    rowNumber: 0,
-    moleNumber: 0,
-  },
-  mutations: {
-    update(state, params) {
-      state.colNumber = Number(params.colNumber);
-      state.rowNumber = Number(params.rowNumber);
-      state.moleNumber = Number(params.moleNumber);
-    }
-  },
-  actions: {},
-  modules: {},
+	state: {
+		colNumber: 0,
+		rowNumber: 0,
+		moleNumber: 0,
+	},
+	mutations: {
+		update(state, params) {
+			state.colNumber = Number(params.colNumber);
+			state.rowNumber = Number(params.rowNumber);
+			state.moleNumber = Number(params.moleNumber);
+		},
+	},
+	actions: {},
+	modules: {},
 });
