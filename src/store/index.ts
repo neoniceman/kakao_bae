f6c31955272a5192ea262b4ac9f12f852f5ff98a
Jungduck Bae <neoniceguy@kobaltlab.com>
2021-05-20 00:22:11 +0900

    레벨 추

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,20 @@
 import { createStore } from "vuex";
 
 export default createStore({
-	state: {
-		colNumber: 0,
-		rowNumber: 0,
-		moleNumber: 0,
-	},
-	mutations: {
-		update(state, params) {
-			state.colNumber = Number(params.colNumber);
-			state.rowNumber = Number(params.rowNumber);
-			state.moleNumber = Number(params.moleNumber);
-		},
-	},
-	actions: {},
-	modules: {},
+  state: {
+    colNumber: 0,
+    rowNumber: 0,
+    moleNumber: 0,
+    level: 0,
+  },
+  mutations: {
+    update(state, params) {
+      state.colNumber = Number(params.colNumber);
+      state.rowNumber = Number(params.rowNumber);
+      state.moleNumber = Number(params.moleNumber);
+      state.level = Number(params.level);
+    },
+  },
+  actions: {},
+  modules: {},
 });
